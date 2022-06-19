ef8adc51f6eb3c37c344781c4ae34bae7ac6ab0f
sybil <sybil@example.com>
2022-06-19 16:33:35 +0400

    explicitly run startVueApp and add store to window

--- chain/vue/src/main.js
+++ chain/vue/src/main.js
@@ -4,6 +4,12 @@ import store from './store'
 import router from './router'
 import vueLib from '@starport/vue'
 
-const app = createApp(App)
-app.config.globalProperties._depsLoaded = true
-app.use(store).use(router).use(vueLib).mount('#app')
+window.vueStore = store
+
+// This is probbly not great, but we want to use the same
+//
+window.startVueApp = function() {
+    const app = createApp(App)
+    app.config.globalProperties._depsLoaded = true
+    app.use(store).use(router).use(vueLib).mount('#app')
+}
