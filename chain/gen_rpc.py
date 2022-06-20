3a864e76f1ed8bc2a725d68a4563a7e2ade16587
sybil singleton <sybil.singleton@protonmail.com>
2022-06-20 03:18:32 +0000

    fix: usr: TXBuilder - add json-editor input type overrides #13

--- chain/gen_rpc.py
+++ chain/gen_rpc.py
@@ -4,7 +4,8 @@ from string import Template
 import json
 
 
-msg_template = Template('''
+msg_template = Template(
+    """
 // Keeper: $mod_keeper
 // Types: $mod_types
 // $keeper_import
@@ -34,9 +35,11 @@ func (rpcservice *RpcService) $function_name(_ *http.Request, msg *$mod_types.$r
 	*response = *r
 	return nil
 }
-''')
+"""
+)
 
-query_template = Template('''
+query_template = Template(
+    """
 // Keeper: $mod_keeper
 // Types: $mod_types
 // $keeper_import
@@ -48,7 +51,8 @@ func (rpcservice *RpcService) $function_name(_ *http.Request, msg *$mod_types.$r
 	*response = *r
 	return nil
 }
-''')
+"""
+)
 
 
 types = set()
@@ -56,72 +60,112 @@ keepers = set()
 code = []
 schemas = {}
 for file_path in sorted(glob.glob("vue/src/**/protomodule.json", recursive=True)):
-    if 'crisis' in file_path: continue
+    if "crisis" in file_path:
+        continue
     data = json.load(open(file_path))
-    for services in data['Pkg']['Services']:
-        for d in services['RPCFuncs']:
-            if services['Name'] == 'Msg':
-                command = f'{ data["Pkg"]["Name"] }/send{ services["Name"] }{ d["Name"] }'
+    for services in data["Pkg"]["Services"]:
+        for d in services["RPCFuncs"]:
+            if services["Name"] == "Msg":
+                command = (
+                    f'{ data["Pkg"]["Name"] }/send{ services["Name"] }{ d["Name"] }'
+                )
 
-            if services['Name'] == 'Query':
+            if services["Name"] == "Query":
                 command = f'{ data["Pkg"]["Name"] }/{ services["Name"] }{ d["Name"] }'
 
-            req_schema = json.load(open(str(Path(file_path).parent / Path('module/types')/ Path(f"{d['RequestType']}.json"))))
-            res_schema = json.load(open(str(Path(file_path).parent / Path('module/types')/ Path(f"{d['ReturnsType']}.json"))))
+            req_schema = json.load(
+                open(
+                    str(
+                        Path(file_path).parent
+                        / Path("module/types")
+                        / Path(f"{d['RequestType']}.json")
+                    )
+                )
+            )
+            res_schema = json.load(
+                open(
+                    str(
+                        Path(file_path).parent
+                        / Path("module/types")
+                        / Path(f"{d['ReturnsType']}.json")
+                    )
+                )
+            )
             schemas[command] = {
-                    "module_name": data["Pkg"]["Name"],
-                    "name": d['Name'],
-                    "service_name": services['Name'],
-                    "request_schema": req_schema,
-                    "resp_schema": res_schema,
-                    "http_rules": d['HTTPRules']
-
-                    }
-            #print(d)
-            service_name = services['Name']
-            if service_name == 'Msg':
+                "module_name": data["Pkg"]["Name"],
+                "name": d["Name"],
+                "service_name": services["Name"],
+                "request_schema": req_schema,
+                "resp_schema": res_schema,
+                "http_rules": d["HTTPRules"],
+            }
+            # print(d)
+            service_name = services["Name"]
+            if service_name == "Msg":
                 template = msg_template
-                function_name = f"{data['Pkg']['Name']}sendMsg{d['Name']}".replace('.','').capitalize()
-                handler_template = Template("handler := $mod_keeper.NewMsgServerImpl(rpcservice.k.$mod_keeper).$keeper_function")
-            elif service_name == 'Query':
+                function_name = f"{data['Pkg']['Name']}sendMsg{d['Name']}".replace(
+                    ".", ""
+                ).capitalize()
+                handler_template = Template(
+                    "handler := $mod_keeper.NewMsgServerImpl(rpcservice.k.$mod_keeper).$keeper_function"
+                )
+            elif service_name == "Query":
                 template = query_template
-                handler_template = Template("""r, err := rpcservice.k.$mod_keeper.$keeper_function(rpcservice.ctx, msg)""")
-                function_name = f"{data['Pkg']['Name']}Query{d['Name']}".replace('.','').capitalize()
+                handler_template = Template(
+                    """r, err := rpcservice.k.$mod_keeper.$keeper_function(rpcservice.ctx, msg)"""
+                )
+                function_name = f"{data['Pkg']['Name']}Query{d['Name']}".replace(
+                    ".", ""
+                ).capitalize()
             else:
-                raise Exception(f'oh no: {service_name}')
-            mod_types=f"{data['Pkg']['Name']}types".lower().replace('.','')
-            mod_keeper=f"{data['Pkg']['Name']}keeper".lower().replace('.','')
+                raise Exception(f"oh no: {service_name}")
+            mod_types = f"{data['Pkg']['Name']}types".lower().replace(".", "")
+            mod_keeper = f"{data['Pkg']['Name']}keeper".lower().replace(".", "")
 
-            if 'dyson' in mod_keeper:
-                keeper_import = data['Pkg']['GoImportName'].replace('/types', '')
-                if service_name == 'Msg':
-                    handler_template = Template("handler := rpcservice.m.$keeper_function")
-                elif service_name == 'Query':
-                    handler_template = Template("""r, err := rpcservice.k.$keeper_function(rpcservice.ctx, msg)""")
+            if "dyson" in mod_keeper:
+                keeper_import = data["Pkg"]["GoImportName"].replace("/types", "")
+                if service_name == "Msg":
+                    handler_template = Template(
+                        "handler := rpcservice.m.$keeper_function"
+                    )
+                elif service_name == "Query":
+                    handler_template = Template(
+                        """r, err := rpcservice.k.$keeper_function(rpcservice.ctx, msg)"""
+                    )
                 types.add(f'{mod_types} "{data["Pkg"]["GoImportName"]}"')
             else:
-                if 'vesting' in mod_keeper:
-                    keeper_import = data['Pkg']['GoImportName'].replace('/types', '')
-                    if service_name == 'Msg':
-                        handler_template = Template("handler := $mod_keeper.NewMsgServerImpl(rpcservice.k.accountKeeper, rpcservice.k.bankkeeper).$keeper_function")
-                elif 'ibcapplication' in mod_keeper:
+                if "vesting" in mod_keeper:
+                    keeper_import = data["Pkg"]["GoImportName"].replace("/types", "")
+                    if service_name == "Msg":
+                        handler_template = Template(
+                            "handler := $mod_keeper.NewMsgServerImpl(rpcservice.k.accountKeeper, rpcservice.k.bankkeeper).$keeper_function"
+                        )
+                elif "ibcapplication" in mod_keeper:
                     # none because it uses the one on the keeper
                     keeper_import = None
-                    if service_name == 'Msg':
-                        handler_template = Template("handler := rpcservice.k.$mod_keeper.$keeper_function")
-                elif ('staking' in mod_keeper) and (service_name == 'Query'):
-                    mod_keeper=f"{data['Pkg']['Name']}querier".lower().replace('.','')
+                    if service_name == "Msg":
+                        handler_template = Template(
+                            "handler := rpcservice.k.$mod_keeper.$keeper_function"
+                        )
+                elif ("staking" in mod_keeper) and (service_name == "Query"):
+                    mod_keeper = f"{data['Pkg']['Name']}querier".lower().replace(
+                        ".", ""
+                    )
                     keeper_import = None
-                elif 'authz' in mod_keeper:
+                elif "authz" in mod_keeper:
                     # none because it uses the one on the keeper
                     keeper_import = None
-                    if service_name == 'Msg':
-                        handler_template = Template("handler := rpcservice.k.$mod_keeper.$keeper_function")
-                elif 'feegrant' in mod_keeper:
-                    keeper_import = data['Pkg']['GoImportName'] + '/keeper'
+                    if service_name == "Msg":
+                        handler_template = Template(
+                            "handler := rpcservice.k.$mod_keeper.$keeper_function"
+                        )
+                elif "feegrant" in mod_keeper:
+                    keeper_import = data["Pkg"]["GoImportName"] + "/keeper"
 
                 else:
-                    keeper_import = data['Pkg']['GoImportName'].replace('/types', '/keeper')
+                    keeper_import = data["Pkg"]["GoImportName"].replace(
+                        "/types", "/keeper"
+                    )
                 if keeper_import:
                     keepers.add(f'{mod_keeper} "{keeper_import}"')
                 types.add(f'{mod_types} "{data["Pkg"]["GoImportName"]}"')
@@ -129,24 +173,24 @@ for file_path in sorted(glob.glob("vue/src/**/protomodule.json", recursive=True)
             template_data = dict(
                 function_name=function_name,
                 mod_types=mod_types,
-                req_type=d['RequestType'],
-                resp_type=d['ReturnsType'],
+                req_type=d["RequestType"],
+                resp_type=d["ReturnsType"],
                 mod_keeper=mod_keeper,
-                keeper_function=d['Name'],
+                keeper_function=d["Name"],
                 keeper_import=keeper_import,
             )
             handler = handler_template.safe_substitute(**template_data)
-            template_data['handler'] = handler
+            template_data["handler"] = handler
 
-            code.append(template.safe_substitute( **template_data))
-
-
-imports = '\n'.join(types | keepers)
-
-funcs ='\n'.join(code)
+            code.append(template.safe_substitute(**template_data))
 
 
-out = (f'''package keeper
+imports = "\n".join(types | keepers)
+
+funcs = "\n".join(code)
+
+
+out = f"""package keeper
 
 import (
     "net/http"
@@ -157,12 +201,37 @@ import (
 )
 
 { funcs }
-''')
+"""
 
 
-with open('x/dyson/keeper/rpcserver.go', 'w') as f:
+with open("x/dyson/keeper/rpcserver.go", "w") as f:
     f.write(out)
 
-with open('vue/src/views/command_schema.json', "w") as f:
-    json.dump(schemas, f, indent=2)
+# override schema to use textareas
+schemas["dyson/sendMsgCreateScript"]["request_schema"]["definitions"][
+    "MsgCreateScript"
+]["properties"]["code"]["format"] = "textarea"
 
+schemas["dyson/sendMsgUpdateScript"]["request_schema"]["definitions"][
+    "MsgUpdateScript"
+]["properties"]["code"]["format"] = "textarea"
+
+schemas["dyson/QueryQueryScript"]["request_schema"]["definitions"][
+    "MsgRun"
+]["properties"]["extra_lines"]["format"] = "textarea"
+
+schemas["dyson/sendMsgRun"]["request_schema"]["definitions"][
+    "MsgRun"
+]["properties"]["extra_lines"]["format"] = "textarea"
+
+schemas["dyson/sendMsgUpdateStorage"]["request_schema"]["definitions"][
+    "MsgUpdateStorage"
+]["properties"]["data"]["format"] = "textarea"
+
+schemas["dyson/sendMsgCreateStorage"]["request_schema"]["definitions"][
+    "MsgCreateStorage"
+]["properties"]["data"]["format"] = "textarea"
+
+
+with open("vue/src/views/command_schema.json", "w") as f:
+    json.dump(schemas, f, indent=2)
