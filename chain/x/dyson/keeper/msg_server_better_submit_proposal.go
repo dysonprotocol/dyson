472972401527284a905ce0332037c11687635489
sybil singleton <sybil.singleton@protonmail.com>
2022-12-16 21:03:17 +0000

    wrap govMsgServer.SubmitProposal in BetterSubmitProposal

--- chain/x/dyson/keeper/msg_server_better_submit_proposal.go
+++ chain/x/dyson/keeper/msg_server_better_submit_proposal.go
@@ -5,13 +5,36 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/org/dyson/x/dyson/types"
+	gov "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
+	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
+	 
+
 )
 
 func (k msgServer) BetterSubmitProposal(goCtx context.Context, msg *types.MsgBetterSubmitProposal) (*types.MsgBetterSubmitProposalResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+	//ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
 
-	return &types.MsgBetterSubmitProposalResponse{}, nil
+	msgs := make([]sdk.Msg, len(msg.Messages))
+	for i, anyJSON := range msg.Messages {
+		var msg sdk.Msg
+		err := k.cdc.UnmarshalInterfaceJSON([]byte(anyJSON), &msg)
+		if err != nil {
+			return nil, err
+		}
+
+		msgs[i] = msg
+	}
+	proposal, err := gov.NewMsgSubmitProposal(msgs, msg.InitialDeposit, msg.Proposer, msg.Metadata)
+	if err != nil {
+		return nil, err
+	}
+	govMsgServer := govkeeper.NewMsgServerImpl(k.cosmosgovv1keeper)
+	resp, err := govMsgServer.SubmitProposal(goCtx, proposal)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.MsgBetterSubmitProposalResponse{ProposalId: resp.ProposalId}, nil
 }
