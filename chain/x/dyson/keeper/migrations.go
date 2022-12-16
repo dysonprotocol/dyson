e7884c3d048432577e483aa059a3da31869c2efe
sybil singleton <sybil.singleton@protonmail.com>
2022-12-16 21:07:40 +0000

    add migrations

---
+++ chain/x/dyson/keeper/migrations.go
@@ -0,0 +1,20 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// Migrator is a struct for handling in-place store migrations.
+type Migrator struct {
+	keeper Keeper
+}
+
+// NewMigrator returns a new Migrator.
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{keeper: keeper}
+}
+
+func (m Migrator) Noop(ctx sdk.Context) error {
+	//noop, no store changes
+	return nil
+}
