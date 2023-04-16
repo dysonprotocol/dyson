3080fb63f178d246dc2e7e5c5b8bb2e0b34813ca
sybil singleton <sybil.singleton@protonmail.com>
2023-04-16 19:41:23 +0000

    add check for name ownership

--- chain/x/names/keeper/msg_server_set_nft_class.go
+++ chain/x/names/keeper/msg_server_set_nft_class.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/nft"
 	"github.com/org/dyson/x/names/types"
 )
@@ -18,6 +19,20 @@ func (k Keeper) SetClass(ctx sdk.Context, class nft.Class) error {
 }
 func (k msgServer) SetNftClass(goCtx context.Context, msg *types.MsgSetNftClass) (*types.MsgSetNftClassResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Check if the value exists
+	name, isFound := k.GetName(
+		ctx,
+		msg.Id, // the name Id is the dyson name
+	)
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "Name not found")
+	}
+
+	// Checks if the the msg owner is the same as the current owner
+	if msg.Owner != name.Owner {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
 
 	class := nft.Class{
 		Id:          msg.Id,
